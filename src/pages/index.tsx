b81374db4e362ba5bd73a2e5e6ffdfad8c0019ba
Guillermo Dolores <guilllermo.dolores@zartis.com>
2022-04-05 22:53:25 -0500

    moved content to src

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione culpa
+      recusandae similique facilis eos exercitationem. Consectetur laborum ea,
+      autem eos repellat quis id illo porro est magni hic, velit quae?
+    </div>
+  );
+};
+
+export default Home;
